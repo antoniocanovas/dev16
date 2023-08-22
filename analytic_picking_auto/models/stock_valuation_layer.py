b6474fe8cde6c3c0192dd5dbe0cb3efc5e5a2fa6
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-08-22 18:53:54 +0200

    analytic picking auto mig

--- analytic_picking_auto/models/stock_valuation_layer.py
+++ analytic_picking_auto/models/stock_valuation_layer.py
@@ -8,3 +8,30 @@ class StockValuationLayer(models.Model):
     _inherit = "stock.valuation.layer"
 
     analytic_line_ids = fields.One2many('account.analytic.line', 'svl_id', string='Analytic Lines', store="True",)
+
+    @api.depends('create_date')
+    def _analytic_picking_auto(self):
+        for record in self:
+            move = record.stock_move_id
+            picking = move.picking_id
+            plan = move.analytic_distribution
+            status = picking.state
+            code = picking.picking_type_id.code
+
+            exist = record.analytic_line_ids
+            name = record.description
+            quantity = record.quantity
+            uom = record.uom_id
+            value = record.value
+
+            if (move.id and picking.id and plan and not exist.ids and status == 'done') and (code in ['incoming','outgoing']):
+              for aa,percent in plan.items():
+                analytic = self.env['account.analytic.account'].search([('id','=',int(aa))])
+                new = self.env['account.analytic.line'].create({'name':name,
+                                                                'account_id':analytic.id,
+                                                                'product_id':record.product_id.id,
+                                                                'product_uom_id':uom.id,
+                                                                'unit_amount':quantity,
+                                                                'amount':value * percent / 100,
+                                                                'svl_id': record.id
+                                                                })
