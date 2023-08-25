2bb01e6cb90a070d0034ef459e9f3712f74d7314
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-08-25 17:17:09 +0200

    risk contract v0

--- account_financial_risk_contract/models/account_move.py
+++ account_financial_risk_contract/models/account_move.py
@@ -8,11 +8,12 @@ class AccountMove(models.Model):
 
     @api.depends('payment_state','state','risk_batch_id','partner_id.risk_total')
     def _get_risk_communication_pending(self):
-        pending = True
-        if self.payment_state not in ['not_paid','in_payment','partial']:  pending = False
-        if self.state not in ['posted']:  pending = False
-        if self.risk_batch_id.id: pending = False
-        if self.partner_id.credit_limit == 0: pending = False
-        self.risk_pending = pending
+        for record in self:
+            pending = True
+            if record.payment_state not in ['not_paid','in_payment','partial']:  pending = False
+            if record.state not in ['posted']:  pending = False
+            if record.risk_batch_id.id: pending = False
+            if record.partner_id.credit_limit == 0: pending = False
+            record.risk_pending = pending
     risk_pending = fields.Boolean('Risk pending', store=True, default=True,
                                   compute='_get_risk_communication_pending')
