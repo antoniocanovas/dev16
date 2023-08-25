5918685ac31eefb8d6b926612c92e635a43b327a
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-08-25 10:37:10 +0200

    risk contract v0

--- account_financial_risk_contract/models/account_move.py
+++ account_financial_risk_contract/models/account_move.py
@@ -1,7 +1,7 @@
 from odoo import _, api, fields, models
 
 
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
+class AccountMove(models.Model):
+    _inherit = 'account.move'
 
     risk_batch_id = fields.Many2one('risk.batch', string='Risk batch', store=True, copy=False)
