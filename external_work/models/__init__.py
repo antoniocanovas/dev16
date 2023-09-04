183555d7c25ea858459c732662f670f721c069d1
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-09-04 22:53:46 +0200

    - external_work

--- external_work/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import product
-from . import external_work
-from . import external_work_line
-from . import hr_expense
--- external_work/models/hr_expense.py
+++
@@ -1,9 +0,0 @@
-from odoo import _, api, fields, models
-
-import logging
-_logger = logging.getLogger(__name__)
-
-class HRExpense(models.Model):
-    _inherit = 'hr.expense'
-
-    external_work_id = fields.Many2one('external.work', store=True, string="External Work")
