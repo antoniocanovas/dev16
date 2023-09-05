60ae3bf6b9e5216e2ce4e0843315fa85df3dd813
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-09-05 12:06:34 +0200

    dev konery crm

--- crm_konery/models/crm_lead2opportunity_partner.py
+++ crm_konery/models/crm_lead2opportunity_partner.py
@@ -4,10 +4,17 @@ from odoo import _, api, fields, models
 class Lead2OpportunityPartner(models.TransientModel):
     _inherit = 'crm.lead2opportunity.partner'
 
-    action = fields.Selection([
+    action_konery = fields.Selection([
+#        ('create', 'Create a new customer'),
         ('exist', 'Link to an existing customer'),
         ('nothing', 'Do not link to a customer')
-        ])
+    ], string='Related Customer', compute='_compute_action', readonly=False, store=True, compute_sudo=False)
+    lead_id = fields.Many2one('crm.lead', 'Associated Lead', required=True)
+
+#    action = fields.Selection([
+#        ('exist', 'Link to an existing customer'),
+#        ('nothing', 'Do not link to a customer')
+#        ])
     @api.model
     def _get_new_selection(self):
         selection = [
