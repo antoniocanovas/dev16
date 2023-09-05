61fcf8699003ddd30ee30919fa021c9f1b2dd76d
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-09-05 12:09:07 +0200

    dev konery crm

--- crm_konery/models/crm_lead2opportunity_partner.py
+++ crm_konery/models/crm_lead2opportunity_partner.py
@@ -5,13 +5,12 @@ class Lead2OpportunityPartner(models.TransientModel):
     _inherit = 'crm.lead2opportunity.partner'
 
     action_konery = fields.Selection([
-#        ('create', 'Create a new customer'),
         ('exist', 'Link to an existing customer'),
         ('nothing', 'Do not link to a customer')
     ], string='Related Customer', compute='_compute_action', readonly=False, store=True, compute_sudo=False)
-    lead_id = fields.Many2one('crm.lead', 'Associated Lead', required=True)
 
 #    action = fields.Selection([
+#        ('create', 'Create a new customer'),
 #        ('exist', 'Link to an existing customer'),
 #        ('nothing', 'Do not link to a customer')
 #        ])
