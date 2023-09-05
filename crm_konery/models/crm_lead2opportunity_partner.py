7a9710a23884de33ee3c211249b9f9917d5f2b79
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-09-05 11:53:50 +0200

    dev konery crm

--- crm_konery/models/crm_lead2opportunity_partner.py
+++ crm_konery/models/crm_lead2opportunity_partner.py
@@ -4,9 +4,15 @@ from odoo import _, api, fields, models
 class CrmLead2opportunityPartner(models.Model):
     _inherit = 'crm.lead2opportunity.partner'
 
-
-    action = fields.Selection([
-#        ('create', 'Create a new customer'),
-        ('exist', 'Link to an existing customer'),
-        ('nothing', 'Do not link to a customer')
-    ], string='Related Customer', compute='_compute_action', readonly=False, store=True, compute_sudo=False)
+    action = fields.Selection(selection='_get_new_selection')
+    @api.model
+    def _get_new_selection(self):
+        selection = [
+        ('text_box', 'Zone de texte à plusieurs lignes'),
+        ('char_box', 'Zone de texte sur une seule ligne'),
+        ('numerical_box', 'Valeur numérique'),
+        ('date', 'Date'),
+        ('datetime', 'Date et heure'),
+        ('simple_choice', 'Choix multiple : une seule réponse')
+        ]
+        return selection
