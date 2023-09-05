d037152af45394afc5b00115585f1e8d2d296edf
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-09-05 12:40:44 +0200

    dev konery crm

--- crm_konery/models/crm_lead.py
+++ crm_konery/models/crm_lead.py
@@ -5,4 +5,3 @@ class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
     power_kw = fields.Float(string='Power (kw)', store=True)
-    prueba = fields.Char('Prueba crm')
--- crm_konery/models/crm_lead2opportunity_partner.py
+++ crm_konery/models/crm_lead2opportunity_partner.py
@@ -3,17 +3,16 @@ from odoo import _, api, fields, models
 class Lead2OpportunityPartner(models.TransientModel):
     _inherit = 'crm.lead2opportunity.partner'
 
-    prueba = fields.Char('Prueba')
     action_konery = fields.Selection([
         ('exist', 'Link to an existing customer'),
         ('nothing', 'Do not link to a customer')
     ], string='Related Customer', compute='_compute_action', readonly=False, store=True, compute_sudo=False)
 
-#    action = fields.Selection([
+    action = fields.Selection([
 #        ('create', 'Create a new customer'),
-#        ('exist', 'Link to an existing customer'),
-#        ('nothing', 'Do not link to a customer')
-#        ])
+        ('exist', 'Link to an existing customer'),
+        ('nothing', 'Do not link to a customer')
+        ])
     @api.model
     def _get_new_selection(self):
         selection = [
