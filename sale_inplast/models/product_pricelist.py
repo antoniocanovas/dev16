b8548ff3f7e7980e17df012d8bbb7dfc3762fbf5
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-09-08 18:11:23 +0200

    inplast sales

--- sale_inplast/models/product_pricelist.py
+++ sale_inplast/models/product_pricelist.py
@@ -11,7 +11,7 @@ class ProductPricelist(models.Model):
     def _get_pricelist_products(self):
         products = []
         for li in self.item_ids:
-            if li.product_id.id not in products.ids:
+            if li.product_id.id not in products:
                 products.append(li.product_id.id)
         self.product_ids = [(6,0,products)]
     product_ids = fields.Many2many('product.product', store=True, compute='_get_pricelist_products')
