fc3304ac4b4ffd0a08860220094817a660938f69
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-09-08 17:47:09 +0200

    inplast sales

---
+++ sale_inplast/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Custom Inplast Sales',
+    'version': '16.0.1.0.0',
+    'category': '',
+    'description': u"""
+Custom Inplast Sales
+""",
+    'author': 'Serincloud',
+    'depends': [
+        'contacts',
+        'sale_management',
+    ],
+    'data': [
+        'views/res_partner_views.xml',
+        'views/sale_order_views.xml',
+    ],
+    'installable': True,
+}
