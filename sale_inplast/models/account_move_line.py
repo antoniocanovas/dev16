e3b0ed63cf3a62f1c46ca010115333d17fd5e9ae
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-09-09 10:13:50 +0200

    inplast sales

--- sale_inplast/models/account_move_line.py
+++ sale_inplast/models/account_move_line.py
@@ -8,4 +8,4 @@ class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
     product_ids = fields.Many2many('product.product', store=False, string='Pricelist products',
-                                   related='pricelist_id.product_ids')
+                                   related='move_id.pricelist_id.product_ids')
