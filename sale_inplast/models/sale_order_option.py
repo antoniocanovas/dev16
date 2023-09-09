f5331f8175e90e980a0880ad5e4f3979a80029f0
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-09-09 08:59:58 +0200

    inplast sales

--- sale_inplast/models/sale_order_option.py
+++ sale_inplast/models/sale_order_option.py
@@ -5,7 +5,7 @@ _logger = logging.getLogger(__name__)
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = 'sale.order.option'
 
     product_ids = fields.Many2many('product.product', store=False, string='Pricelist products',
                                    related='order_id.pricelist_id.product_ids')
