cf25f8eac24744034bae3a0cbad969e4ec4e88b3
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-09-14 17:22:08 +0200

    subs rel task 0

--- subscription_recurrent_task/models/account_move.py
+++ subscription_recurrent_task/models/account_move.py
@@ -1,7 +1,6 @@
 from odoo import _, api, fields, models
-from odoo.exceptions import UserError, ValidationError
 
-class AccountMoveLine(models.Model):
+class AccountMove(models.Model):
     _inherit = 'account.move'
 
     @api.depends('state')
@@ -18,11 +17,10 @@ class AccountMoveLine(models.Model):
                         create_task = True
                     if create_task == True:
                         name = li.subscription_id.name + " - " + str(li.subscription_start_date)
-                        raise UserError(name)
                         newtask = self.env['project.task'].create({
                             'name':name,
                             'partner_id': record.partner_id.id,
                             'project_id': li.subscription_id.subscription_project_id.id,
-                            'user_id': li.subscription_id.subscription_project_id.user_id.id,
+                            'user_ids': [(6, 0, [li.subscription_id.subscription_project_id.user_id.id])],
                         })
                         li['task_id'] = newtask.id
