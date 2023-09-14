98a63facf1f5d913b228f815e8182e69e96f43e1
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-09-14 17:01:53 +0200

    subs rel task 0

--- subscription_recurrent_task/models/account_move.py
+++ subscription_recurrent_task/models/account_move.py
@@ -6,20 +6,21 @@ class AccountMoveLine(models.Model):
 
     @api.depends('state')
     def _create_task_from_subscription(self):
-        if (self.state in ['draft','posted']):
-            for li in self.invoice_line_ids:
-                create_task = False
-                # If subscription need task on DRAFT and not created before:
-                if not (li.task_id.id) and (li.subscription_id.task_create == 'draft') and (self.state == 'draft'):
-                    create_task = True
-                # If subscription need task on POSTED and not created before:
-                if not (li.task_id.id) and (li.subscription_id.task_create == 'posted') and (self.state == 'posted'):
-                    create_task = True
-                if create_task == True:
-                    name = li.subscription_id.name + " - " + str(li.subscription_start_date)
-                    newtask = self.env['project.task'].create({
-                        'name':name,
-                        'partner_id': self.partner_id.id,
-                        'project_id': li.subscription_id.subscription_project_id.id,
-                    })
-                    li['task_id'] = newtask.id
+        for record in self:
+            if (record.state in ['draft','posted']):
+                for li in record.invoice_line_ids:
+                    create_task = False
+                    # If subscription need task on DRAFT and not created before:
+                    if not (li.task_id.id) and (li.subscription_id.task_create == 'draft') and (record.state == 'draft'):
+                        create_task = True
+                    # If subscription need task on POSTED and not created before:
+                    if not (li.task_id.id) and (li.subscription_id.task_create == 'posted') and (record.state == 'posted'):
+                        create_task = True
+                    if create_task == True:
+                        name = li.subscription_id.name + " - " + str(li.subscription_start_date)
+                        newtask = self.env['project.task'].create({
+                            'name':name,
+                            'partner_id': record.partner_id.id,
+                            'project_id': li.subscription_id.subscription_project_id.id,
+                        })
+                        li['task_id'] = newtask.id
