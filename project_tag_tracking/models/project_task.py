a4698bc0eeabccc17664e01764f4fb8e2c770709
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-09-24 03:17:51 +0200

    task_tag_tracking v1

--- project_tag_tracking/models/project_task.py
+++ project_tag_tracking/models/project_task.py
@@ -6,14 +6,13 @@ from odoo.exceptions import UserError
 class ProjectTask(models.Model):
     _inherit = 'project.task'
 
-#    tag_ids = fields.Many2many(tracking=100)
-    tag_tracking = fields.Text('Tags tracking', store=True, tracking=100)
-    tag_prev_ids = fields.Many2many(store=True, copy=False, string="Previous tags",
-        comodel_name="project.tags",
-        relation='task_tags_rel',
-        column1='task_id',
-        column2='tag_id',
-    )
+    #   Versión antigua: tag_ids = fields.Many2many(tracking=100)
+    tag_prev_ids = fields.Many2many(store=True, copy=False, string="Previous tags", readonly="1",
+                                    comodel_name="project.tags",
+                                    relation='task_tags_rel',
+                                    column1='task_id',
+                                    column2='tag_id',
+                                    )
 
 
     def update_task_tracking(self):
@@ -21,10 +20,16 @@ class ProjectTask(models.Model):
         for tag in self.tag_ids.ids:
             if tag not in self.tag_prev_ids.ids:
                 tag = self.env['project.tags'].search([('id', '=', tag)])
-                tagtracking += "(+) " + tag.name + "\n"
+                tagtracking += "<p>(+) " + tag.name + "</p>"
         for tag in self.tag_prev_ids.ids:
             if tag not in self.tag_ids.ids:
                 tag = self.env['project.tags'].search([('id', '=', tag)])
-                tagtracking += "(-) " + tag.name + "\n"
+                tagtracking += "<p>(-) " + tag.name + "</p>"
         if tagtracking != "":
             self.write({'tag_prev_ids': [(6, 0, self.tag_ids.ids)], 'description': tagtracking})
+            new_note = self.env['mail.message'].create({'body': tagtracking,
+                                                        'message_type': 'comment',
+                                                        'model': 'project.task',
+                                                        'res_id': self.id,
+                                                        })
+            
