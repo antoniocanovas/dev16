d263184e09ecb4a19a8b1ab4110f4b8115097421
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-09-27 18:21:21 +0200

    product_att_bom v0

--- product_attribute_bom/models/product_attribute_value.py
+++ product_attribute_bom/models/product_attribute_value.py
@@ -9,6 +9,6 @@ class ProductAttributeValue(models.Model):
     set_template_id = fields.Many2one('set.template', string='Set template', store=True, copy=False)
     
     def _get_company_bom_attribute_id(self):
-        self.company_attribute_bom_id = self.user.company_id.product_attribute_id.id
+        self.company_attribute_bom_id = self.env.user.company_id.product_attribute_id.id
     company_attribute_bom_id = fields.Many2one('product.attribute', string='Company bom attribute',
                                                store=False, compute='_get_company_bom_attribute_id')
