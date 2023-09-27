af081a7419ae17945a3ec444d181ac16b595ea65
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-09-27 18:15:37 +0200

    product_att_bom v0

--- product_attribute_bom/models/product_attribute_value.py
+++ product_attribute_bom/models/product_attribute_value.py
@@ -7,3 +7,7 @@ class ProductAttributeValue(models.Model):
     _inherit = 'product.attribute.value'
 
     set_template_id = fields.Many2one('set.template', string='Set template', store=True, copy=False)
+    
+    def _get_company_bom_attribute_id(self):
+        self.company_attribute_bom_id = self.user.company_id.product_attribute_id.id
+    company_attribute_bom_id = fields.Many2one('Company', store=False, compute='_get_company_bom_attribute_id')
