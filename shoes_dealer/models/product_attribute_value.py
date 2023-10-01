aea150f4b7509897c97f7f04006e36afff7690f3
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-10-01 02:39:29 +0200

    shoes_dealer v0

--- shoes_dealer/models/product_attribute_value.py
+++ shoes_dealer/models/product_attribute_value.py
@@ -9,8 +9,8 @@ class ProductAttributeValue(models.Model):
     set_template_id = fields.Many2one('set.template', string='Set template', store=True, copy=False)
     
     def _get_set_hidden(self):
-        company_attribute = self.env.user.company_id.product_attribute_id
+        company_bom_attribute = self.env.user.company_id.bom_attribute_id
         set_hidden = False
-        if (company_attribute.id != self.attribute_id.id): set_hidden = True
+        if (company_bom_attribute.id != self.attribute_id.id): set_hidden = True
         self.set_hidden = set_hidden
     set_hidden = fields.Boolean('Set hidden', store=False, compute='_get_set_hidden')
