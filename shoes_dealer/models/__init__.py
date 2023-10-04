8a78ec6b8b3f4b0a31f0899315392b23ebce4187
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-10-05 00:53:08 +0200

    shoes dealer v0

--- shoes_dealer/models/__init__.py
+++ shoes_dealer/models/__init__.py
@@ -4,4 +4,5 @@ from . import product_template
 from . import product_product
 from . import res_company
 from . import product_attribute_value
+from . import sale_order
 from . import sale_order_line
