cb8049e9763925b7686958c1cd9915d86f96ed1c
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-10-05 01:16:56 +0200

    shoes dealer v0

--- shoes_dealer/models/product_attribute_value.py
+++ shoes_dealer/models/product_attribute_value.py
@@ -11,6 +11,6 @@ class ProductAttributeValue(models.Model):
     def _get_set_hidden(self):
         company_bom_attribute = self.env.user.company_id.bom_attribute_id
         set_hidden = False
-        if (company_bom_attribute.id != self.attribute_id.id): set_hidden = True
+        if (company_bom_attribute.id in [self.attribute_id.id, False]): set_hidden = True
         self.set_hidden = set_hidden
     set_hidden = fields.Boolean('Set hidden', store=False, compute='_get_set_hidden')
