edd5447817c0fc37f4fb7661aecf5649bccc15a3
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-10-05 00:58:20 +0200

    shoes dealer v0

--- custom_azarey/models/product_pricelist.py
+++ custom_azarey/models/product_pricelist.py
@@ -4,8 +4,8 @@
 from odoo import fields, models, api
 
 class ProductPricelist(models.Model):
-    _inherit = ['product.pricelist', 'mail.thread', 'mail.activity.mixin']
-#    _inherit = ['product.pricelist']
+#    _inherit = ['product.pricelist', 'mail.thread', 'mail.activity.mixin']
+    _inherit = ['product.pricelist']
 
     pnt_campaign_id = fields.Many2one('project.project', string='Campaign', store=True, copy=False, tracking='1')
     pnt_pre_margin_amount = fields.Monetary('Pre margin', store=True, copy=True, tracking='1')
