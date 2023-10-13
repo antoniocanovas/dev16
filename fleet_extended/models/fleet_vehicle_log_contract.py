42df4b8caa6138752fc8e6381be86cbbe2876ea7
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-10-13 13:14:59 +0200

    fleet v1

--- fleet_extended/models/fleet_vehicle_log_contract.py
+++ fleet_extended/models/fleet_vehicle_log_contract.py
@@ -30,7 +30,11 @@ class FleetVehicleLogContract(models.Model):
 
     @api.depends('contract_km','vehicle_id.odometer', 'vehicle_id.odometer_ids.value')
     def _get_pending_contract_km(self):
-        self.pending_km = self.contract_km - self.vehicle_id.odometer
+        total = 0
+        odometer = self.env['fleet.vehicle.odometer'].search([('vehicle_id', '=', self.vehicle_id.id)], order='value desc')[0]
+        if odometer_date.id:
+            total = self.contract_km - odometer.value
+        self.pending_km = total
     pending_km = fields.Integer('Pending km', store=True, compute='_get_pending_contract_km')
 
     @api.depends('contract_km','expiration_date','start_date')
@@ -43,9 +47,9 @@ class FleetVehicleLogContract(models.Model):
     @api.depends('start_date', 'vehicle_id.odometer', 'vehicle_id.odometer_ids.value')
     def _get_annual_consumed_km(self):
         annual_consumed_km, bisiestos = 0, [2024,2028,2032,2036,2040,2044,2048,2052,2056,2060]
-        odometer_date = self.env['fleet.vehicle.odometer'].search([('vehicle_id', '=', self.vehicle_id.id)], order='value desc')[0]
+        odometer = self.env['fleet.vehicle.odometer'].search([('vehicle_id', '=', self.vehicle_id.id)], order='value desc')[0]
         if odometer_date.id:
-            dif = odometer_date.date - self.start_date
+            dif = odometer.date - self.start_date
             total_days = dif.days - self.year_leap_count
             annual_consumed_km = self.vehicle_id.odometer / ( total_days / 365 )
         self.annual_consumed_km = annual_consumed_km
