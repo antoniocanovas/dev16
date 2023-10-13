6d17a245eefa54780216850710c8d0036080fc8e
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-10-13 12:46:09 +0200

    fleet v1

--- fleet_extended/models/fleet_vehicle.py
+++ fleet_extended/models/fleet_vehicle.py
@@ -4,4 +4,4 @@ from datetime import datetime, date
 class FleetVehicleLogContract(models.Model):
     _inherit = 'fleet.vehicle'
 
-    odometer_ids = fields.One2many('fleet.vehicle.odometer', 'vehicle_id' store=True, copy=False)
+    odometer_ids = fields.One2many('fleet.vehicle.odometer', 'vehicle_id', store=True, copy=False)
