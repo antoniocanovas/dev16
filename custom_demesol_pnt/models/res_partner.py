372c7aa493258884568ea6cd7bf5e80661676e35
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-10-13 08:19:27 +0200

    demesol v0

--- custom_demesol_pnt/models/res_partner.py
+++ custom_demesol_pnt/models/res_partner.py
@@ -7,8 +7,8 @@ class ProductBrand(models.Model):
     _inherit = "res.partner"
 
     # Para borrar después de la migración:
-    mig_agente = fields.Char(string='mig_agente_pnt', store=True)
-    mig_agente2 = fields.Char(string='mig_agente2_pnt', store=True)
-    mig_nif = fields.Char(string='mig_nif_pnt', store=True)
-    mig_banco = fields.Char(string='mig_banco_pnt', store=True)
-    mig_iban = fields.Char(string='mig_iban_pnt', store=True)
+    mig_agente_pnt = fields.Char(string='mig_agente_pnt', store=True)
+    mig_agente2_pnt = fields.Char(string='mig_agente2_pnt', store=True)
+    mig_nif_pnt = fields.Char(string='mig_nif_pnt', store=True)
+    mig_banco_pnt = fields.Char(string='mig_banco_pnt', store=True)
+    mig_iban_pnt = fields.Char(string='mig_iban_pnt', store=True)
