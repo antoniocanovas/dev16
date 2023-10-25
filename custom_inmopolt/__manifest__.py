fb0ac9043101a75232068a9b172a5866b7d51a9a
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-10-25 18:14:39 +0200

    customaz

---
+++ custom_inmopolt/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2023 Serincloud ( https://www.ingenieriacloud.com )
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Custom Inmopolt',
+    'version': '16.0.1.0.0',
+    'category': 'Sales',
+    "license": "AGPL-3",
+    'website': "https://ingenieriacloud.com",
+    'summary': 'Inmopolt Customs',
+    'author': 'Serincloud',
+    'depends': [
+        'sale_management',
+    ],
+    'data': [
+
+    ],
+    'installable': True,
+    'application': False,
+}
---
+++ custom_inmopolt/models/sale_order.py
@@ -0,0 +1,12 @@
+from odoo import _, api, fields, models
+
+
+class ProductProduct(models.Model):
+    _inherit = 'sale.order'
+
+    def name_get(self):
+        result = []
+        for order in self:
+            name = order.name + ' ' + order.ref
+            result.append((order.id, name))
+        return result
