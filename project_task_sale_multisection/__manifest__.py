725f4bbdc01d50940aab33bcd3002a1468f47e24
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-10-30 09:10:38 +0100

    inelga 2mods

--- project_task_sale_multisection/__manifest__.py
+++ project_task_sale_multisection/__manifest__.py
@@ -1,12 +1,13 @@
 {
     'name': 'Project Task Sale Multisection',
-    'version': '14.0.1.0.0',
+    'version': '16.0.1.0.0',
     'category': '',
     'description': u"""
      Añade a la tarea la sección del pedido de venta.
 """,
     'author': 'Serincloud',
     'depends': [
+        'sale_management',
         'sale_order_multisection',
         'sale_timesheet',
     ],
