dfc532907fbe8f5684c549ee480faf1d431d983a
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-10-31 15:46:40 +0100

    shoes

--- shoes_dealer/models/product_product.py
+++ shoes_dealer/models/product_product.py
@@ -9,8 +9,9 @@ class ProductProduct(models.Model):
     # Pares por variante de producto, se usará en el cálculo de tarifas y líneas de venta:
     def _get_shoes_product_product_pair_count(self):
         count = 1
-        if self.bom_ids.ids:
-            count = self.bom_ids[0].pairs_count
+        bom = self.env['mrp.bom'].search([('product_id','=',self.id)])
+        if bom.ids:
+            count = bom[0].pairs_count
         self.pairs_count = count
     pairs_count = fields.Integer('Pairs', store=False, compute='_get_shoes_product_product_pair_count')
 
