97ecb642a6a724c525b38586810e0e48172bc0a6
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-11-02 17:43:58 +0100

    cups

--- power_cups_pnt/models/power_cups.py
+++ power_cups_pnt/models/power_cups.py
@@ -23,10 +23,10 @@ class PowerCUPS(models.Model):
 
     @api.depends('pnt_partner_id','pnt_partner_id.parent_id')
     def _get_cups_customer(self):
-        customer = self.pnt_customer_id
+        customer = self.pnt_partner_id
         if customer_id.parent_id.id:
-            customer = customer_id.parent_id.id
-        self.pnt_customer_id = customer
+            customer = customer_id.parent_id
+        self.pnt_customer_id = customer.id
     pnt_customer_id = fields.Many2one('res.partner', string='Customer', store=True, compute='_get_cups_customer')
 
     pnt_kw_fw       = fields.Float('Photovoltaic (kw)', store=True, copy=True, tracking=True)
