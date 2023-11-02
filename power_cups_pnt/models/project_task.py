6706caa09caebeb28966160c04506c08432eabeb
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-11-02 18:02:37 +0100

    cups

--- power_cups_pnt/models/project_task.py
+++ power_cups_pnt/models/project_task.py
@@ -5,6 +5,6 @@ _logger = logging.getLogger(__name__)
 
 
 class ProjectProject(models.Model):
-    _inherit = 'project.project'
+    _inherit = 'project.task'
 
     pnt_power_cups_id = fields.Many2one('power.cups', string='CUPS', store=True, related='project_id.pnt_power_cups_id')
