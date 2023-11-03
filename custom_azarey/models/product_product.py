1b9111001baaa46c0aa4315ac8403c2fc2a399ce
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-11-03 17:00:56 +0100

    aza-contrains

--- custom_azarey/models/product_product.py
+++ custom_azarey/models/product_product.py
@@ -2,7 +2,7 @@
 
 from odoo import fields, models, api
 from odoo.exceptions import UserError
-
+from datetime import datetime, date
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
@@ -14,3 +14,12 @@ class ProductProduct(models.Model):
         for record in self:
             if (record.active == False) and (record.pnt_sale_line_ids.ids):
                 raise UserError('Producto con ventas en curso, cancélalas primero.')
+
+    def _delete_product_archived_in_sale_line(self):
+        for li in self.pnt_sale_line_ids:
+            if li.product_id.id == record.id:
+                name = li.product_id.name + ", cantidad: " + str(li.product_uom_qty) + \
+                       " => Cancelado el " + str(date.now())
+                newnote = env['sale.order.line'].create(
+                    {'display_type': 'line_note', 'name': name, 'order_id': li.order_id.id})
+                li.unlink()
