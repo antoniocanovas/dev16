cd09c3880ec2d570eb9791a6e3c3466610841cdb
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-11-04 17:35:09 +0100

    so states

--- custom_inplast/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import pnt_coa
-from . import product_template
--- sale_inplast/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import sale_order_line
-from . import product_pricelist
-from . import product_pricelist_item
-from . import sale_order_option
-from . import account_move_line
