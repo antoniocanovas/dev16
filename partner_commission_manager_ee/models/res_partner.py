6a26329e5238373a73e6166b496178f2f97b47d6
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-11-20 12:56:56 +0100

    commissions_manager v0

--- partner_commission_manager_ee/models/res_partner.py
+++ partner_commission_manager_ee/models/res_partner.py
@@ -9,11 +9,10 @@ class ResPartner(models.Model):
 
     # Campos para ampliar funcionalidad de comisionistas:
     manager_id = fields.Many2one('res.partner', 'Manager', domain=[('grade_id', '!=', False)], tracking=True)
-    manager_commission_plan_id = fields.Many2one('commission.plan', 'Manager Plan',
-                                                 default=_default_manager_commission_plan,
-                                                 tracking=True)
 
     @api.model
     def _default_manager_commission_plan(self):
         return self.manager_id.grade_id.default_commission_plan_id
-
+    manager_commission_plan_id = fields.Many2one('commission.plan', 'Manager Plan',
+                                                 default=_default_manager_commission_plan,
+                                                 tracking=True)
