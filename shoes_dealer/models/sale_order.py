667f4b2b1c78734d58ac9dd02ebddaee1059ad77
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-07 17:47:45 +0100

    sd2

--- shoes_dealer/models/sale_order.py
+++ shoes_dealer/models/sale_order.py
@@ -10,10 +10,9 @@ class SaleOrder(models.Model):
     def _get_shoes_pair_count(self):
         for record in self:
             count = 0
-            if record.id:
-                for li in record.order_line:
-                    count += li.pairs_count
+            for li in record.order_line:
+                count += li.pairs_count
             record['pairs_count'] = count
-    pairs_count = fields.Integer('Pairs', store=False, compute='_get_shoes_pair_count', default=0)
+    pairs_count = fields.Integer('Pairs', store=False, compute='_get_shoes_pair_count')
 
     campaign_id = fields.Many2one('project.project', string="Campaign", store=True, copy=True, tracking=10)
