785b65443733f71df3dbec6e4c9c39382474137f
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-08 13:18:56 +0100

    -azarey

--- custom_azarey/reports/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import pnt_project_task_report
-from . import pnt_product_label_report
-
--- custom_azarey/wizard/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import pnt_product_pricelist_report_wizard
-from . import pnt_project_task_report_wizard
-from . import product_label_layout
