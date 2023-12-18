562d5a8bc11dad72c2a195a552a8cabc201c72d1
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-18 19:09:54 +0100

    report

--- shoes_dealer/models/sale_report.py
+++ shoes_dealer/models/sale_report.py
@@ -34,5 +34,6 @@ class SaleReport(models.Model):
                     ('product_tmpl_id', '=', record.product_tmpl_id.id),
                     ('attribute_id', '=', color_attribute.id)]).product_template_value_ids[0].product_attribute_value_id
             record['color_id'] = color_value.id
-    color_id = fields.Many2one('product.attribute.value', string='Color', store=True, default=0,
+    color_id = fields.Many2one('product.attribute.value', string='Color', store=True,
+                               default=lambda self: self.company.color_attribute_id.id,
                                compute='get_sale_report_color')
