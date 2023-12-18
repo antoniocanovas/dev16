901243d5c24eb26868cd5d44e94d4abe9726a32b
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-18 19:16:36 +0100

    report

--- shoes_dealer/models/sale_report.py
+++ shoes_dealer/models/sale_report.py
@@ -1,21 +1,17 @@
 # Copyright 2023 Serincloud SL - Ingenieriacloud.com
 
 from odoo import fields, models, api
-from odoo.exceptions import UserError, ValidationError
+
 
 class SaleReport(models.Model):
     _inherit = "sale.report"
-
-    # Informes de ventas:
-
-
-#   @api.depends('product_id')
-#    def _get_shoes_pair_count(self):
-    #        for record in self:
-    #        pairs_count = 1
-    #        #if record.product_id.pairs_count: pairs_count = record.product_id.pairs_count
-    #        record['pairs_count'] = pairs_count * record.product_uom_qty
-    #    pairs_count = fields.Integer('Pairs', store=True, compute='_get_shoes_pair_count')
+    @api.depends('product_id')
+    def _get_shoes_sale_line_pair_count(self):
+        for record in self:
+            pairs_count = 1
+            if record.product_id.pairs_count: pairs_count = record.product_id.pairs_count
+            record['pairs_count'] = pairs_count * record.product_uom_qty
+    pairs_count = fields.Integer('Pairs', store=True, compute='_get_shoes_pair_count')
 
     @api.depends('product_id')
     def get_sale_report_color(self):
@@ -34,6 +30,4 @@ class SaleReport(models.Model):
                     ('product_tmpl_id', '=', record.product_tmpl_id.id),
                     ('attribute_id', '=', color_attribute.id)]).product_template_value_ids[0].product_attribute_value_id
             record['color_id'] = color_value.id
-    color_id = fields.Many2one('product.attribute.value', string='Color', store=True,
-                               default=lambda self: self.company.color_attribute_id.id,
-                               compute='get_sale_report_color')
+    color_id = fields.Many2one('product.attribute.value', string='Color', store=True, compute='get_sale_report_color')
