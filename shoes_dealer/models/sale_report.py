915549f78495bcaaa229186e382fe17611311348
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-12-19 10:21:20 +0100

    report

--- shoes_dealer/models/sale_report.py
+++ shoes_dealer/models/sale_report.py
@@ -7,31 +7,16 @@ class SaleReport(models.Model):
     _inherit = "sale.report"
 
 
+    color_attribute_id = fields.Many2one('product.attribute.value', string='Color', store=True,
+                                         related='product_id.color_attribute_id')
+
+    size_attribute_id = fields.Many2one('product.attribute.value', string='Size', store=True,
+                                         related='product_id.size_attribute_id')
+
     @api.depends('product_id')
     def _get_shoes_pair_count(self):
         for record in self:
             pairs_count = 1
             if record.product_id.pairs_count: pairs_count = record.product_id.pairs_count
             record['pairs_count'] = pairs_count * record.product_uom_qty
-    pairs_count = fields.Integer('Pairs', store=False, compute='_get_shoes_pair_count')
-
-#    @api.depends('product_id')
-    #   def get_sale_report_color(self):
-    #    for record in self:
-    #        color_attribute = self.env.user.company_id.color_attribute_id
-
-            # Para buscar el color:
-    #            color_value = self.env['product.template.attribute.value'].search([
-    #            ('product_tmpl_id', '=', record.product_tmpl_id.id),
-    #            ('id', 'in', record.product_id.product_template_variant_value_ids.ids),
-    #            ('attribute_id', '=', color_attribute.id)]).product_attribute_value_id
-
-            # Caso de que sólo haya un COLOR, no existe el registro anterior PTAV, buscamos en la línea atributo de PT:
-    #       if not color_value.id:
-    #            color_value = self.env['product.template.attribute.line'].search([
-    #                ('product_tmpl_id', '=', record.product_tmpl_id.id),
-    #                ('attribute_id', '=', color_attribute.id)]).product_template_value_ids[0].product_attribute_value_id
-    #        record['color_id'] = color_value.id
-#    color_id = fields.Many2one('product.attribute.value', string='Color', store=False, compute='get_sale_report_color')
-    color_attribute_id = fields.Many2one('product.attribute.value', string='Color', store=True,
-                                         related='product_id.color_attribute_id')
+    pairs_count = fields.Integer('Pairs', store=True, compute='_get_shoes_pair_count')
