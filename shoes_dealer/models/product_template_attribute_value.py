e25079a125123d401a699b1afd54ae753c2ded87
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-02-19 03:00:38 +0100

    sd3

--- shoes_dealer/models/product_template_attribute_value.py
+++ shoes_dealer/models/product_template_attribute_value.py
@@ -19,7 +19,7 @@ class ProductTemplateAttributeValue(models.Model):
             ptal = record.attribute_line_id
             value = record.product_attribute_value_id
 
-            if (record.product_tmpl_id.is_assortment) and (pt_single_id.id):
+            if (record.product_tmpl_id.is_assortment) and (pt_single.id):
                 # Los colores se pueden borrar directamente ya que tienen relacion directa con pt_single:
                 if (attribute.id == color_attribute):
                     ptal_color_single = env['product.template.attribute.line'].search(
