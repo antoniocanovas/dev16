5d2a0293f0e84c757218a3fbed29f197906a085c
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-02-25 00:55:18 +0100

    sd4

--- shoes_dealer/models/product_product.py
+++ shoes_dealer/models/product_product.py
@@ -18,8 +18,10 @@ class ProductProduct(models.Model):
                 for li in record.product_template_variant_value_ids:
                     if li.attribute_id == self.env.company.color_attribute_id:
                         value = li.product_attribute_value_id.id
-#                    elif li.attribute_id == self.env.company.color_attribute_id:
-#                        value = li.value_ids[0]
+            if len(record.product_variant_ids.ids) == 1:
+                for li in record.product_tmpl_id.attribute_line_ids:
+                    if li.attribute_id == self.env.company.color_attribute_id:
+                        value = li.value_ids[0]
             record['color_attribute_id'] = value
     color_attribute_id = fields.Many2one('product.attribute.value', string='Color', store=True,
                                          compute='_get_color_attribute_value')
