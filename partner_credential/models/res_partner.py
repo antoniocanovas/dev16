c00071f83a354bf2ef88f02860807a7d557c016d
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-05-03 17:02:31 +0200

    credentialsv2

--- partner_credential/models/res_partner.py
+++ partner_credential/models/res_partner.py
@@ -16,7 +16,7 @@ class PartnerCredentialsFields(models.Model):
     )
 
     def action_view_credentials(self):
-        action = self.env.ref("partner_credential.action_partner_credentials").read()[
+        action = self.env.ref("partner_credential.action_partner_credential").read()[
             0
         ]
         return action
