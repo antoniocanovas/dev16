dc8acac22b7b9fd458cc5c4a5e6078b8d0799c9b
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-05-03 17:29:50 +0200

    credentialsv2

--- partner_credential/__manifest__.py
+++ partner_credential/__manifest__.py
@@ -3,17 +3,18 @@
     'summary': """
         Nuevo modelo para documentar usuario y contraseña de aplicaciones.
         """,
-    'author': "Pedro Guirao",
+    'author': "Antonio Cánovas",
     'license': 'AGPL-3',
     'website': "https://ingenieriacloud.com",
     'category': 'Tools',
-    'version': '14.0.1.0.0',
+    'version': '16.0.1.0.0',
     'depends': [
         'contacts',
         'hr',
     ],
     'data': [
         'views/partner_credential_views.xml',
+        'views/partner_credential_type_views.xml',
         'views/menu_views.xml',
         'views/res_partner_views.xml',
         'security/user_groups.xml',
