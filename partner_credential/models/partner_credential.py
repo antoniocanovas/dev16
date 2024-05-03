08159bea69264369471d9d3418c9ec9afb5dc97b
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-05-03 18:26:18 +0200

    credentialsv2

--- partner_credential/models/partner_credential.py
+++ partner_credential/models/partner_credential.py
@@ -21,6 +21,10 @@ class PartnerCredential(models.Model):
 
     department_categ_ids = fields.Many2many(related='category_id.department_ids', string='Default users')
     department_ids = fields.Many2many("hr.department", string="Departments", tracking=100)
+    @api.depends('password')
+    def _get_pass_updated(self):
+        self.pass_updated = self.pass_updated +1
+    pass_updated = fields.Integer("Pass updated", store=True, tracking=100, compute="_get_pass_updated")
 
     @api.depends('department_ids', 'department_ids.member_ids', 'department_categ_ids', 'department_categ_ids.member_ids')
     def _get_department_users(self):
