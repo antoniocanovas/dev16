6dd17f66da44da09f0f91d7f0e2e1a246449513c
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-05-03 17:54:16 +0200

    credentialsv2

--- partner_credential/models/partner_credential.py
+++ partner_credential/models/partner_credential.py
@@ -32,7 +32,7 @@ class PartnerCredential(models.Model):
                 if emp.user_id.id not in users:
                     users.append(emp.user_id.id)
         self.user_ids = [(6,0,users)]
-    user_ids = fields.Many2many("res.users", string="Users", store=True, compute="_get_department_users")
+    user_ids = fields.Many2many("res.users", string="Users", store=True, compute="_get_department_users", index=True)
 
     def _user_can_edit(self):
         for record in self:
