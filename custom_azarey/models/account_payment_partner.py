732c67445d477b00c52f0c9d965bbbaf84ac8106
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-06-12 15:37:12 +0200

    shoes4

--- custom_azarey/models/account_payment_partner.py
+++
@@ -1,9 +0,0 @@
-# Copyright
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-
-from odoo import fields, models, api
-
-class AccountPaymentMode(models.Model):
-    _inherit = 'account.payment.mode'
-
-    pnt_print_reference_invoice = fields.Boolean(string='Print Reference on invoice', default=False)
