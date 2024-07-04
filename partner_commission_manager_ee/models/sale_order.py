938b1e95c02dbe884ac83aa7083d67488af048ce
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-07-04 08:40:46 +0200

    commissionv2

--- partner_commission_manager_ee/models/sale_order.py
+++ partner_commission_manager_ee/models/sale_order.py
@@ -14,10 +14,12 @@ class SaleOrder(models.Model):
 
     referrer_id = fields.Many2one(
         "res.partner",
+        index=True,
+        store=True,
         compute="_get_default_commission_referrer",
     )
 
-    # Método para heredar MANAGER dependiendo del COMISIONISTA (referrer):
+    # Método para heredar MANAGER dependiendo del COMISIONISTA (referrer_id):
     @api.onchange("referrer_id")
     def _get_commission_manager_id(self):
         self.manager_id = self.partner_id.referrer_id.manager_id.id
